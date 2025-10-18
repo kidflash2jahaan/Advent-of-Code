8665aff1cef3d96ef82097ad60bb2ff242ec6971
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2025-10-17 20:46:37 -0700

    2016.1.bonus

--- 2016.1.py
+++ 2016.1.py
@@ -3,6 +3,10 @@ puzzle_input = "R4, R4, L1, R3, L5, R2, R5, R1, L4, R3, L5, R2, L3, L4, L3, R1, 
 x = 0
 y = 0
 compass = 0
+first_x = 0.1
+first_y = 0.1
+previous_x_list = [0]
+previous_y_list = [0]
 
 for instruction in puzzle_input.split(", "):
     direction = instruction[0]
@@ -16,15 +20,26 @@ for instruction in puzzle_input.split(", "):
         compass = 0
     if compass < 0:
         compass = 3
-    
-    match compass:
-        case 0:
-            y += distance
-        case 1:
-            x += distance
-        case 2:
-            y -= distance
-        case 3:
-            x -= distance
 
-print(abs(x)+abs(y))
+    for i in range(distance):
+        match compass:
+            case 0:
+                y += 1
+            case 1:
+                x += 1
+            case 2:
+                y -= 1
+            case 3:
+                x -= 1
+        
+
+        for j in range(len(previous_x_list)):
+            if previous_x_list[j] == x and previous_y_list[j] == y and first_x == 0.1 and first_y == 0.1:
+                first_x = x
+                first_y = y
+        
+        previous_x_list.append(x)
+        previous_y_list.append(y)
+        
+
+print(abs(first_x)+abs(first_y))
